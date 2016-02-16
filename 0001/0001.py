98384d8694f1221c2d3ceff63732a5c2834af227
sikuquanshu123 <sikuquanshu123@hotmail.com>
2016-02-16 10:20:11 +0800

    两种生成随机数
    random.choice(str)
    random.randint(a,b)

--- 0001/0001.py
+++ 0001/0001.py
@@ -5,8 +5,8 @@ def cal(num=1,length=10):
     strings='abcdefghijklmnopgrstuvwxyzABCDEFGHIJKLMNOPGRSTUVWXYZ1234567890'
     for j in range(num):
         for i in range(length):
-            x=random.randint(0,len(strings))
-            print(strings[i],end="")
+            x=random.randint(0,len(strings)-1)
+            print(strings[x],end="")
         print()
 
 def cho(num=1,length=10):
@@ -15,7 +15,8 @@ def cho(num=1,length=10):
         for i in range(length):
             print(random.choice(strings),end="")
         print()
-    
-
+            
 if __name__=="__main__":
+    cal(5)
+    print("...............................................")
     cho(5)
